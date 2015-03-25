f57064a0f06dc253dd8a19f121c18366c3fea13a
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-03-25 10:13:41 -0700

    Add create user feature specs

---
+++ spec/features/create_user_pages_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'the create a user process' do
+  it 'has a link on the homepage to create a new user' do
+    visit root_path
+    expect(page).to have_content "Create an account"
+  end
+
+  it 'has a link on the homepage to create a new user' do
+    visit root_path
+    click_on "Create an account"
+    expect(page).to have_content "Email"
+  end
+
+  it 'will create an account when filled in with properly formatted info' do
+    visit root_path
+    click_on "Create an account"
+    fill_in "Email", :with => "clem@test.com"
+    fill_in "Password", :with => "test"
+    fill_in "Confirm Password", :with => "test"
+    click_on "Create Account"
+    expect(page).to have_content "Success"
+  end
+end
