6edd4c6b94033d3398f448050688336b73d3110f
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-03-25 15:12:58 -0700

    Add log out functionality

--- spec/features/log_out_pages_spec.rb
+++ spec/features/log_out_pages_spec.rb
@@ -1,12 +1,28 @@
 require 'rails_helper'
 
-describe 'the log user process' do
+describe 'the log out a user process' do
+  it 'doesn\'t display the logout button when not logged in' do
+    FactoryGirl.create(:user)
+    visit unauthenticated_root_path
+    expect(page).to have_no_content "Log out"
+  end
+
   it 'displays the logout button when logged in' do
     FactoryGirl.create(:user)
-    visit authenticated_root_path
+    visit unauthenticated_root_path
     fill_in "Email", :with => "clem@test.com"
     fill_in "Password", :with => "testtest"
     click_on "Log in"
     expect(page).to have_content "Log out"
+  end
+
+  it 'logs out a user when the button is pressed' do
+    FactoryGirl.create(:user)
+    visit unauthenticated_root_path
+    fill_in "Email", :with => "clem@test.com"
+    fill_in "Password", :with => "testtest"
+    click_on "Log in"
+    click_on "Log out"
+    expect(page).to have_content "Log in"
   end
 end
