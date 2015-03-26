fcb99f510fbf0a8e740652072f10a2edf3fe3630
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-03-26 14:26:30 -0700

    Add display of Book link on non-owned lairs

--- spec/features/create_booking_spec.rb
+++ spec/features/create_booking_spec.rb
@@ -10,12 +10,12 @@ describe 'the book a lair process' do
   it 'will show a book lair button when user does not own lair' do
     FactoryGirl.create(:lair, :user_id => user.id)
     visit lairs_path
-    expect(page).to have_content 'Book'
+    expect(page).to have_no_content 'Book'
   end
 
   it 'will not show a book lair button when user owns lair' do
     FactoryGirl.create(:lair, :user_id => 999999)
     visit lairs_path
-    expect(page).to have_no_content 'Book'
+    expect(page).to have_content 'Book'
   end
 end
