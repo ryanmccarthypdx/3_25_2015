5319f22734f9100c583924f0a90c8ce838da87eb
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-03-26 11:10:21 -0700

    Add tests for user and lair relationship

--- spec/models/lair_spec.rb
+++ spec/models/lair_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-# RSpec.describe Lair, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+describe Lair do
+  it { should belong_to :user }
+end
